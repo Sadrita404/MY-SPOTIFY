f5d2e9b8a6893ae34bc94e2f4451798f01f69a97
Sadrita Neogi <sadritaneogi@gmail.com>
2026-05-07 12:31:22 +0530

    Integrate OpenWeatherMap and refactor code structure
    Refactor code to include OpenWeatherMap integration and improve structure.

--- FIRMWARE/code_my_spotify.cpp
+++ FIRMWARE/code_my_spotify.cpp
@@ -1,79 +1,122 @@
 #include <Arduino.h>
-#include <ArduinoJson.h>
-#include <Adfruit_ST7735.h>
-#include <Adafruit_GFX.h>
-#include <SPI.h>
 #include <WiFi.h>
+#include <SPI.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_ST7735.h>
+#include <OpenWeatherMapCurrent.h>
 #include <spotify.h>
 
-#define TFT_CS 1
-#define TFT_RST 2
-#define TFT_DC 3
-#define TFT_SCLK 4
-#define TFT_MOSI 5
-#define BTN_BACK    8
-#define BTN_PP      7
-#define BTN_FWD     6
+#define TFT_CS    1
+#define TFT_RST   2 
+#define TFT_DC    3
+#define BTN_PP    7 
+#define BTN_FWD   6 
 
-char* SSID = "our_wifi_ssid";
-char* PASSWORD = "our_wifi_password";
-const char* CLIENT_ID = "spotify_client_id THAT YOU GET FROM SPOTIFY DASHBOARD";
-const char* CLIENT_SECRET = "spotify_client_secret THAT YOU GET FROM SPOTIFY DASHBOARD";
+const char* SSID = "your_wifi_ssid";
+const char* PASSWORD = "your_wifi_password";
+const char* CLIENT_ID = "your_spotify_id";
+const char* CLIENT_SECRET = "your_spotify_secret";
+const char* OW_KEY = "your_openweathermap_api_key";
+const char* OW_LOC = "your_city_id";
 
-String lastArtist;
-String lastTrackname;
-
+Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_RST);
 spotify sp(CLIENT_ID, CLIENT_SECRET);
-Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_MOSI, TFT_SCLK, TFT_RST);
- 
-void setup(){
-    Serial.begin(115200);
-    tft.initR(INITR_BLACKTAB);?? the type of screan you are yuseing for this projrct
-    tft.setRotation(1);// 1 make landscapr mode and 0 make portrait mode
-    Serial.println("TFT Initialized");
-    tft.fillScreen(ST7735_BLACK);// set bg color into black
+OpenWeatherMapCurrentData weatherData;
+OpenWeatherMapCurrent weatherClient;
 
-    WiFi.begin(SSID, PASSWORD);
-    Serial.print("Connecting to WiFi!!!");
-    while (WiFi.status()!= WL_CONNECTED)
+bool showWeather = false;
+unsigned long lastWeatherUpdate = 0;
+String lastTrack = "";
+String lastArtist = "";
 
-    {
-        delay(1000);
-        Serial.print("!");
-    }
-    Serial.printf("\n Connected to WiFi!\n");
-    sp.begin();
-    while(!sp.is_auth()){
-        sp.handle_client();
-    }
-    Serial.println("Authenticated with Spotify!!!");
-
-
-// In setup():
-pinMode(BTN_BACK, INPUT_PULLUP);
-pinMode(BTN_PP,   INPUT_PULLUP);
-pinMode(BTN_FWD,  INPUT_PULLUP);
+void updateWeather() {
+  weatherClient.updateCurrentById(&weatherData, OW_KEY, OW_LOC);
+  lastWeatherUpdate = millis();
 }
 
-void loop()
-{
-    String currentArtist = sp.current_artist_names();
-    String currentTrackname = sp.current_track_name();
+void displayWeather() {
+  tft.fillScreen(ST77XX_BLACK);
+  tft.setCursor(10, 10);
+  tft.setTextColor(ST77XX_YELLOW);
+  tft.setTextSize(2);
+  tft.print("Weather");
+  
+  tft.setCursor(10, 40);
+  tft.setTextColor(ST77XX_WHITE);
+  tft.setTextSize(3);
+  tft.print(String(weatherData.temp, 1));
+  tft.print("C");
 
-    if (lastArtist != currentArtist && currentArtist != "Something went wrong" && !currentArtist.isEmpty()) {
-        tft.fillScreen(ST77XX_BLACK);
-        lastArtist = currentArtist;
-        Serial.println("Artist: " + lastArtist);
-        tft.setCursor(10,10);
-        tft.write(lastArtist.c_str());
-    }
-
-    if (lastTrackname != currentTrackname && currentTrackname != "Something went wrong" && currentTrackname != "null") {
-        lastTrackname = currentTrackname;
-        Serial.println("Track: " + lastTrackname);
-        tft.setCursor(10,40);
-        tft.write(lastTrackname.c_str());
-    }
-    delay(2000);
+  tft.setCursor(10, 80);
+  tft.setTextSize(1);
+  tft.print(weatherData.description);
 }
- 
+
+void displaySpotify() {
+  String currentArtist = sp.current_artist_names();
+  String currentTrack = sp.current_track_name();
+
+  if (lastTrack != currentTrack && currentTrack != "null") {
+    tft.fillScreen(ST77XX_BLACK);
+    tft.setCursor(10, 10);
+    tft.setTextColor(ST77XX_GREEN);
+    tft.setTextSize(1);
+    tft.print("Spotify:");
+    tft.setCursor(10, 35);
+    tft.setTextColor(ST77XX_WHITE);
+    tft.setTextSize(2);
+    tft.print(currentTrack);
+    tft.setCursor(10, 70);
+    tft.setTextColor(ST77XX_CYAN);
+    tft.setTextSize(1);
+    tft.print(currentArtist);
+    lastTrack = currentTrack;
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(BTN_PP, INPUT_PULLUP);
+  pinMode(BTN_FWD, INPUT_PULLUP);
+
+  tft.initR(INITR_BLACKTAB);
+  tft.setRotation(1);
+  tft.fillScreen(ST77XX_BLACK);
+
+  WiFi.begin(SSID, PASSWORD);
+  while (WiFi.status() != WL_CONNECTED) { delay(500); }
+  
+  sp.begin();
+  while(!sp.is_auth()) { sp.handle_client(); }
+  
+  updateWeather();
+}
+
+void loop() {
+  if (digitalRead(BTN_PP) == LOW && digitalRead(BTN_FWD) == LOW) {
+    showWeather = !showWeather;
+    tft.fillScreen(ST77XX_BLACK);
+    lastTrack = ""; 
+    delay(500);
+  }
+
+  if (showWeather) {
+    if (millis() - lastWeatherUpdate > 600000) {
+      updateWeather();
+    }
+    displayWeather();
+  } else {
+    displaySpotify();
+    
+    if (digitalRead(BTN_FWD) == LOW) {
+      // Add sp.next() here if library supports
+      delay(300);
+    }
+    if (digitalRead(BTN_PP) == LOW) {
+      // Add sp.pause() here if library supports
+      delay(300);
+    }
+  }
+  
+  delay(100);
+}
